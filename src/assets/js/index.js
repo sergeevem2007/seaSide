11ea1ea2798296f5eb7fa9c676bda24b38e63ad3
sergeevem2007@gmail.com <sergeevem2007@gmail.com>
2021-12-16 21:10:44 +0400

    доб интерактив для якорных ссылок

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // проигрывание видео
 const roomsVideo = document.querySelector('.rooms__video');
 const roomsPlay = document.querySelector('.rooms__play');
@@ -12,3 +14,20 @@ roomsVideo.addEventListener('ended', () => {
   roomsPlay.style.zIndex = "2";
   roomsVideo.style.zIndex = "-2";
 });
+
+//плавная прокурутка к якорным ссылкам
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    const blockID = anchor.getAttribute('href').substr(1);
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+};
+
